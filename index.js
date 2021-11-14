a4a486be85c47046a908ad7e1ba5f6f5308976b7
zarif007 <zarifhuq786@gmail.com>
2021-11-14 17:46:27 +0600

    Added few endpoints

--- index.js
+++ index.js
@@ -36,25 +36,29 @@ async function run(){
 
         app.get('/games/onsale', async(req, res) => {
             const query = {"discount": { $gt: 0 }};
-            const games = await gamesCollection.find(query).toArray();
+            let games = await gamesCollection.find(query).toArray();
+            games = games.slice(0, 6);
             res.json(games);
         });
 
         app.get('/games/free', async(req, res) => {
             const query = {"price": 0};
-            const games = await gamesCollection.find(query).toArray();
+            let games = await gamesCollection.find(query).toArray();
+            games = games.slice(0, 3);
             res.json(games);
         });
 
         app.get('/games/bestseller', async(req, res) => {
             const query = {"soldcopies": { $gt: 100000 }};
-            const games = await gamesCollection.find(query).toArray();
+            let games = await gamesCollection.find(query).toArray();
+            games = games.slice(0, 4);
             res.json(games);
         });
 
         app.get('/games/toprated', async(req, res) => {
             const query = {"rate": { $gt: 8 }};
-            const games = await gamesCollection.find(query).toArray();
+            let games = await gamesCollection.find(query).toArray();
+            games = games.slice(0, 6);
             res.json(games);
         });
 
@@ -68,6 +72,13 @@ async function run(){
         app.post('/users', async(req, res) => {
             const user = req.body;
             const result = await usersCollection.insertOne(user);
+            console.log(result);
+            res.json(result);
+        })
+
+        app.post('/games', async(req, res) => {
+            const user = req.body;
+            const result = await gamesCollection.insertOne(user);
             console.log(result);
             res.json(result);
         })
@@ -88,19 +99,18 @@ async function run(){
             res.send(result);
         })
 
-        app.post('/reviews', async(req, res) => {
-            const review = req.body;
-            console.log(review);
-            const result = await reviewsCollection.insertOne(review);
-            res.send(result);
-        })
 
         app.get('/orders/:id', async(req, res) => {
             const id = req.params.id;
-            const query = {clientId: id};
+            let query = {clientId: id};
+            const user = await usersCollection.findOne({fireBaseId: id});
+            if(user.role === 'Admin')
+                query = {};
             const orders = await ordersCollection.find(query).toArray();
             res.json(orders);
         })
+
+
 
         // DELETE a order 
         app.delete('/orders/:id', async(req, res) => {
@@ -126,6 +136,33 @@ async function run(){
             res.json(result);
         });
 
+        app.post('/reviews', async(req, res) => {
+            const review = req.body;
+            const result = await reviewsCollection.insertOne(review);
+            res.send(result);
+        })
+
+        app.get('/reviews', async(req, res) => {
+            const cursor = reviewsCollection.find({});
+            let reviews = await cursor.toArray();
+            reviews = reviews.slice(0, 6);
+            res.send(reviews);
+        })
+
+        app.get('/userinfo/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {fireBaseId: id};
+            const user = await usersCollection.findOne(query);
+            res.send(user);
+        })
+
+        app.patch('/makeadmin', async(req, res) => {
+            const email = req.body.email;
+            const query = {email: email};
+            console.log('email', email);
+            const result = await usersCollection.updateOne(query, { $set: {role: "Admin" }});
+            res.json(result);
+        });
 
 
     } finally {
